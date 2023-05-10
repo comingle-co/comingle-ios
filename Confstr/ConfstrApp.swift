f26e0743a7d04058f9b3e7034a402b3b1c06bcd0
Terry Yiu <963907+tyiu@users.noreply.github.com>
2023-05-09 22:23:42 -0400

    Add initial Xcode project

---
+++ Confstr/ConfstrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfstrApp.swift
+//  Confstr
+//
+//  Created by Terry Yiu on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ConfstrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
