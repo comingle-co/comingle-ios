95911545b7d486140c470b6a30be1490440fe9ab
Terry Yiu <963907+tyiu@users.noreply.github.com>
2024-07-31 09:34:10 -0400

    Add MapKitAutocompleteManager but do not use it yet until privacy concerns are thought through

---
+++ Comingle/Models/MapKitAutocompleteManager.swift
@@ -0,0 +1,34 @@
+//
+//  MapKitAutocompleteManager.swift
+//  Comingle
+//
+//  Created by Terry Yiu on 7/31/24.
+//
+
+import Foundation
+import MapKit
+
+class AutocompleteManager: NSObject, ObservableObject, MKLocalSearchCompleterDelegate {
+    @Published var searchText = ""
+    @Published var completions: [MKLocalSearchCompletion] = []
+
+    private var searchCompleter: MKLocalSearchCompleter
+
+    override init() {
+        self.searchCompleter = MKLocalSearchCompleter()
+        super.init()
+        self.searchCompleter.delegate = self
+    }
+
+    func updateSearchResults() {
+        searchCompleter.queryFragment = searchText
+    }
+
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        completions = completer.results
+    }
+
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        print("Error completing search: \(error.localizedDescription)")
+    }
+}
